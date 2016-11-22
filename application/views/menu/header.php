735687c71118f5241e2811fd68967154ce503108
Unknown <t4kumi00@students.oamk.fi>
2016-11-22 09:03:47 +0200

    navbar laitettu

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -9,6 +9,22 @@
 
 <ul>
 
+	<nav class="navbar navbar-inverse">
+  	<div class="container-fluid">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="#">Pa-pank</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?php echo base_url(''); ?>">Etusivu</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li> 
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Kirjaudu ulos</a></li>
+    </ul>
+  </div>
+</nav>
+
 	<li><a href="<?php echo base_url(''); ?>">Etusivu</a></li>
 	<li><a href="<?php echo site_url('asiakas/lisaa_asiakas'); ?>">Lisää asiakas</a></li>
 	<li><a href="<?php echo site_url('asiakas/nayta_asiakas'); ?>">Asiakaslista</a></li>
