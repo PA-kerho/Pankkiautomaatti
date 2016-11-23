b408534e2114127bb8f1656cc5cb5e247f300c64
Unknown <t4kumi00@students.oamk.fi>
2016-11-23 14:45:40 +0200

    niinpä

--- application/views/menu/sisalto.php
+++ application/views/menu/sisalto.php
@@ -1,5 +1,5 @@
 
 <?php $this->load->view('menu/header');?>
 <?php $this->load->view($sivun_sisalto);?>
-<div class="taustakuva-image-container"><img class="img-responsive taustakuva-image" src="<?php echo base_url('Bank.png')?>"> </div>
+<div class="taustakuva-image-container"><img class="img-rounded" src="<?php echo base_url('Bank.png')?>" width="1700" height="850" position="center"> </div>
 <?php $this->load->view('menu/footer');?>
