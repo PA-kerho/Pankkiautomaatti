4b2aa83bc6765aadaec1b345277f9576d7506bb9
Tuomas <t4putu01@students.oamk.fi>
2016-11-23 13:58:28 +0200

    Taustakuvitusta

--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -14,24 +14,24 @@
 <ul>
 
 <nav class="navbar navbar-inverse">
-<div class="container-fluid">
-  	<div class="navbar-header">
+	<div class="container-fluid">
+	<div class="navbar-header">
     	<a class="navbar-brand" href="#">Pa-pank</a>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="<?php echo base_url(''); ?>">Etusivu</a></li>
-      
-	      <li class="dropdown">
+
+    	<ul class="nav navbar-nav">
+      		<li class="active"><a href="<?php echo base_url(''); ?>">Etusivu</a></li>
+      		<li class="dropdown">
 	        <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" href="#">Asiakkaat
 	        <span class="caret"></span></a>
 	        <ul class="dropdown:hover dropdown-menu">
 	        	<li><a href="<?php echo site_url('asiakas/nayta_asiakas'); ?>">Tarkastele, muokkaa, poista</a></li>
 	        	<li><a href="<?php echo site_url('asiakas/lisaa_asiakas'); ?>">Uusi asiakas</a></li>
 	          
-	        </ul>
-	      </li>
+	    </ul>
+	    </li>
       
-	      <li class="dropdown">
+	    <li class="dropdown">
 	        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Tilit
 	        <span class="caret"></span></a>
 	        <ul class="dropdown:hover dropdown-menu">  
@@ -39,24 +39,25 @@
 	          <li><a href="<?php echo site_url('Kortti_Tili/nayta_tili'); ?>">Tarkastele, muokkaa, poista</a></li>
 	          <li><a href="<?php echo site_url('Kortti_Tili/lisaa_tili'); ?>">Uusi tili</a></li>
 	        
-	        </ul>
-	      </li>
+	    </ul>
+	    </li>
 
-	      <li class="dropdown">
+	    <li class="dropdown">
 	        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Kortit
 	        <span class="caret"></span></a>
-	        <ul class="dropdown:hover dropdown-menu">
+	    <ul class="dropdown:hover dropdown-menu">
 
 	        	<li><a href="<?php echo site_url('Kortti_Tili/nayta_kortti'); ?>">Tarkastele, muokkaa, poista</a></li>	
 	        	<li><a href="<?php echo site_url('Kortti_Tili/lisaa_kortti'); ?>">Kortit</a></li>
 	          
-	        </ul>
-	      </li>
+	    </ul>
+	    </li>
 
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="<?php echo site_url('Kirjaudu/logout'); ?>"><span class="glyphicon glyphicon-log-out"></span> Kirjaudu ulos</a></li>
-    </ul>
+    	</ul>
+
+    	<ul class="nav navbar-nav navbar-right">
+      	<li><a href="<?php echo site_url('Kirjaudu/logout'); ?>"><span class="glyphicon glyphicon-log-out"></span> Kirjaudu ulos</a></li>
+   		</ul>
 </div>
 </nav>
 </ul>
