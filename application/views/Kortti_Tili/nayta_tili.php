ab50f6c5ef9ea4f8348b1f32f8f74b2c4748ca0e
Unknown <t4kumi00@students.oamk.fi>
2016-11-24 13:48:33 +0200

    PerkPerjantai

--- application/views/Kortti_Tili/nayta_tili.php
+++ application/views/Kortti_Tili/nayta_tili.php
@@ -15,9 +15,8 @@
 		echo '<tr>
 		<td>'.$rivi['ID'].'</td>
 		<td>'.$rivi['IBAN'].'</td>
-		<td>'.$rivi['TilinNimi'].'</td>
+		<td>'.$rivi['TilinNimi'].'</td>';
 
-<<<<<<< HEAD
 		echo '<td>
 				<div class="dropdown">
 					<button class ="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Liitetyt kortit
@@ -26,22 +25,8 @@
 							<li> <a href="http://localhost/Pankkiautomaatti/index.php/Kortti_Tili/nayta_liitetyt_kortit?id='.$rivi['ID'].'">Liitetyt kortit</a>
 							</li>
 						</ul></div></form></td>';
-				//echo $rivi["ID"];
-		/*echo '<td>
-				<div class="dropdown">
-					<button class ="btn btn-primary dropdown-toggle" type="button"'*/
-=======
-		<td>';
->>>>>>> master
-
-		echo '<div class="dropdown">
-				<button class ="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Liitetyt kortit
-
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-				<li> <a href="http://localhost/Pankkiautomaatti/index.php/Kortti_Tili/nayta_liitetyt_kortit?id='.$rivi['ID'].'">Liitetyt kortit</a>
-				</li>
-				</ul></div></form></td>';
+				
+		
 
 
 
@@ -54,16 +39,10 @@
 
 			echo '<input type="submit" name="btnLukitse" class="btn btn-muokkaa btn-xs" value="Lukitse"/>';
 			}
-
-<<<<<<< HEAD
-								echo '<input type="submit" name="btnLukitse" class="btn btn-muokkaa btn-xs" value="Lukitse"/>';
 							}
 			echo '</form></td></tr><br>';
 
-=======
-			echo '</form></td></tr>';
->>>>>>> master
-			}
+			
 ?>;
 
 
