51202f46791bff72043a7e7ed5fa04e9e5661b8b
Unknown <t4kumi00@students.oamk.fi>
2016-11-24 11:53:17 +0200

    jep

--- application/views/Kortti_Tili/nayta_tili.php
+++ application/views/Kortti_Tili/nayta_tili.php
@@ -14,14 +14,16 @@
 		<td>'.$rivi['ID'].'</td>
 		<td>'.$rivi['IBAN'].'</td>
 		<td>'.$rivi['TilinNimi'].'</td>
-		<td>';
+		';
 
-		echo '<div class="dropdown">
-				<button class ="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Liitetyt kortit
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-				<li> <a href="http://localhost/Pankkiautomaatti/index.php/Kortti_Tili/nayta_liitetyt_kortit?id='.$rivi['ID'].'">Liitetyt kortit</a>
-				</li></ul></div></form></td>';
+		echo '<td>
+				<div class="dropdown">
+					<button class ="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Liitetyt kortit
+					<span class="caret"></span></button>
+						<ul class="dropdown-menu">
+							<li> <a href="http://localhost/Pankkiautomaatti/index.php/Kortti_Tili/nayta_liitetyt_kortit?id='.$rivi['ID'].'">Liitetyt kortit</a>
+							</li>
+						</ul></div></form></td>';
 				//echo $rivi["ID"];
 
 
